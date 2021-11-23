fe62f60e69cd8c6a457a3fd90e437ec107c60114
rsedighi <ramin-og@NL-SF-RAMIN.local>
2021-11-23 13:11:38 -0800

    sending to Ameer

--- plugins/netlify-plugin-email-sendgrid/index.js
+++ plugins/netlify-plugin-email-sendgrid/index.js
@@ -4,7 +4,7 @@ module.exports = {
       const sgMail = require("@sendgrid/mail");
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
-        to: "mickael@netlify.com", // Change to your recipient
+        to: "ameer@netlify.com", // Change to your recipient
         from: "ramin+sendgrid@semoneytronic.com", // Change to your verified sender
         subject: "Automated Email sent from Ramin's Sendgrid Build Plugin",
         text: process.env.DEPLOY_URL,
