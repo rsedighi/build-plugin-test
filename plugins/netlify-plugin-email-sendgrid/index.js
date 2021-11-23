1bcb4ef81d6fde4a244863408196dc735ae3cc53
rsedighi <ramin-og@NL-SF-RAMIN.local>
2021-11-23 14:21:01 -0800

    using Environment variable for everything

--- plugins/netlify-plugin-email-sendgrid/index.js
+++ plugins/netlify-plugin-email-sendgrid/index.js
@@ -4,9 +4,9 @@ module.exports = {
       const sgMail = require("@sendgrid/mail");
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
-        to: "ameer@netlify.com", // Change to your recipient
-        from: "ramin+sendgrid@semoneytronic.com", // Change to your verified sender
-        subject: "Automated Email sent from Ramin's Sendgrid Build Plugin",
+        to: process.env.EMAIL_TO, // Change to your recipient
+        from: process.env.APPROVED_SENDER, // Change to your verified sender
+        subject: process.env.EMAIL_SUBJECT,
         text: process.env.DEPLOY_URL,
       };
       try {
