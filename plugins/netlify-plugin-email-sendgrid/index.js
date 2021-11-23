6876c2718b709885e506550b2609368658038dd3
rsedighi <ramin-og@NL-SF-RAMIN.local>
2021-11-23 12:55:49 -0800

    sendgrid code updated

--- plugins/netlify-plugin-email-sendgrid/index.js
+++ plugins/netlify-plugin-email-sendgrid/index.js
@@ -1,26 +1,24 @@
-const sgMail = require('@sendgrid/mail')
 module.exports = {
-    
-    onSuccess: () => {
-        console.log('onSuccess: I run on build success 🎉');
-            sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-            const msg = {
-            to: 'ramin@netlify.com', // Change to your recipient
-            from: 'ramin+sendgrid@netlify.com', // Change to your verified sender
-            subject: 'New Cirque Deploy URL',
-            text: process.env.DEPLOY_URL,
-            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-            }
-            console.log(sgMail)
-            console.log(msg)
-            sgMail
-            .send(msg)
-            .then(() => {
-                console.log('Email sent')
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-            console.log(process.env.DEPLOY_URL)
-    }
-}
+    async onSuccess() {
+      console.log("onSuccess: I run on build success 🎉");
+      const sgMail = require("@sendgrid/mail");
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: "ramin@netlify.com", // Change to your recipient
+        from: "ramin+sendgrid@netlify.com", // Change to your verified sender
+        subject: "New Cirque Deploy URL",
+        text: process.env.DEPLOY_URL,
+      };
+      try {
+        await sgMail.send(msg);
+        console.log("Email sent");
+      } catch (error) {
+        console.error(error);
+  
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      }
+      console.log(process.env.DEPLOY_URL);
+    },
+  };
