42e6fcdbc9bd6446114ee6b736ed93403f901ba5
rsedighi <ramin-og@MacBook-Pro-3.local>
2023-08-10 08:59:22 -0700

    first commit

--- plugins/netlify-plugin-email-sendgrid/index.js
+++ plugins/netlify-plugin-email-sendgrid/index.js
@@ -1,24 +1,35 @@
 module.exports = {
-    async onSuccess() {
-      console.log("onSuccess: I run on build success 🎉");
-      const sgMail = require("@sendgrid/mail");
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const msg = {
-        to: process.env.EMAIL_TO, // Change to your recipient
-        from: process.env.APPROVED_SENDER, // Change to your verified sender
-        subject: process.env.EMAIL_SUBJECT,
-        text: process.env.DEPLOY_URL,
-      };
-      try {
-        await sgMail.send(msg);
-        console.log("Email sent");
-      } catch (error) {
-        console.error(error);
+    async onPreBuild() {
+      console.log("onSuccess: I run  onPreBuild 🎉");
+
+      const fs = require("fs")
+
+      const filenames = fs.readdirSync(__dirname);
   
-        if (error.response) {
-          console.error(error.response.body);
-        }
-      }
-      console.log(process.env.DEPLOY_URL);
+      console.log("\nCurrent directory filenames:");
+      filenames.forEach(file => {
+        console.log(file);
+      });
+
+      
+      // const sgMail = require("@sendgrid/mail");
+      // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      // const msg = {
+      //   to: process.env.EMAIL_TO, // Change to your recipient
+      //   from: process.env.APPROVED_SENDER, // Change to your verified sender
+      //   subject: process.env.EMAIL_SUBJECT,
+      //   text: process.env.DEPLOY_URL,
+      // };
+      // try {
+      //   await sgMail.send(msg);
+      //   console.log("Email sent");
+      // } catch (error) {
+      //   console.error(error);
+  
+      //   if (error.response) {
+      //     console.error(error.response.body);
+      //   }
+      // }
+      // console.log(process.env.DEPLOY_URL);
     },
   };
